57279d2728ae062af98399f1a1478a53c425c876
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-05-07 23:22:00 +0530

    [JD]Add. adding migration for CartTable

---
+++ BookStoreApplication/Repository/Migrations/20200507174755_initial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CartTable",
+                columns: table => new
+                {
+                    CartID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CartBookID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartTable", x => x.CartID);
+                    table.ForeignKey(
+                        name: "FK_CartTable_BookStore_CartBookID",
+                        column: x => x.CartBookID,
+                        principalTable: "BookStore",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartTable_CartBookID",
+                table: "CartTable",
+                column: "CartBookID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartTable");
+        }
+    }
+}
