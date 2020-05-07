96b1b45743f5908fc5d8bf029ea5dc587cd57812
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-05-07 23:09:08 +0530

    [JD]Add. adding BookStoreModel folder and CartModel

---
+++ BookStoreApplication/Model/Model/BookStoreModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+using System.Text;
+
+namespace Model
+{
+    public class BookStoreModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookID { get; set; }
+
+       
+        public string BookTittle { get; set; }
+
+       
+        public string AuthorName { get; set; }
+
+       
+        public double Price { get; set; }
+
+       
+        public string Summary { get; set; }
+
+      
+        public string BookImage { get; set; }
+
+        
+        public int BookCount { get; set; }
+
+    }
+}
