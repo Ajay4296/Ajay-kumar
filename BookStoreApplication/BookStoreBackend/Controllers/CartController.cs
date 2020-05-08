f029ae0d400476c91a0aa26a8a5869a63648ebfd
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-05-08 12:04:01 +0530

    [JD]Add. adding ICartRepository,CartRepository and cartController

---
+++ BookStoreApplication/BookStoreBackend/Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Manager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly IBookManager bookManager;
+
+        public CartController(IBookManager bookManager)
+        {
+            this.bookManager = bookManager;
+        }
+
+        [Route("AddCart")]
+        [HttpPost]
+        public Task<IActionResult> AddCart(IBookManager bookManager)
+        {
+            return bookManager.
+        }
+    }
+}
---
+++ BookStoreApplication/Repository/Repository/CartRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Model;
+using Repository.DBContext;
+
+namespace Repository.Repository
+{
+    public class CartRepository : ICartRepositorycs
+    {
+        private readonly UserDbContext userDbContext;
+        public Task<int> AddCart(CartModel cartModel)
+        {
+            userDbContext.CartTable.Add(cartModel);
+            var result = userDbContext.SaveChangesAsync();
+            return result;
+        }
+    }
+}
