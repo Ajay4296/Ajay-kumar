60d39ca50e3f12e9968120587ba3d9f1e798bb8a
shreyashkaushal <shreyashkaushal10@gmail.com>
2020-05-14 01:33:23 +0530

    [Shreyash] Refactor. refactor displayBook component after merge error

--- bookstoreui/src/components/DisplayBooks.jsx
+++ bookstoreui/src/components/DisplayBooks.jsx
@@ -5,20 +5,11 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-<<<<<<< HEAD
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import logo from '../logo.svg';
-import Tooltip from '@material-ui/core/Tooltip'; 
-
-class DisplayBooks extends Component {
-=======
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip'; 
 
 class DisplayBooks extends Component {
     
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
     render() {
         return (
             <>
@@ -40,16 +31,6 @@ class DisplayBooks extends Component {
                         this.props.books.map((ele) => {
                             return (
                                 <>
-<<<<<<< HEAD
-                                    <Card className='note-card' >
-                                    <Tooltip title={ele.description}>
-                                        <CardActionArea
-                                            onMouseEnter={this.props.bookMouseEnterHandler}
-                                            onMouseLeave={this.props.bookMouseLeaveHandler}
-                                        >
-                                            <img id='img' src={logo} />
-                                            {/* <CardMedia
-=======
                                     <Card className='note-card' key={ele.bookID}>
                                         
                                         <Tooltip  title={ele.summary}> 
@@ -59,61 +40,35 @@ class DisplayBooks extends Component {
                                             {
                                                 /* <CardMedia
                                             
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                                 image={logo}
                             /> */}
                                             <CardContent id='card-content'>
                                                 <Typography gutterBottom variant="h5" component="h2">
-<<<<<<< HEAD
-                                                    {ele.bookName}
-=======
                                                      {ele.bookTittle}
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                                                 </Typography>
                                                 <Typography id='note-content' variant="body2" color="textSecondary" component="p">
                                                     by: {ele.authorName}
                                                 </Typography>
                                                 <Typography id='note-content' variant="body2" color="textSecondary" component="p">
-<<<<<<< HEAD
-                                                ₹ {ele.price}
-=======
                                                     ₹ {ele.price}
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                                                 </Typography>
                                             </CardContent>
                                         </CardActionArea>
                                         </Tooltip>
                                         <CardActions>
-<<<<<<< HEAD
-                                            <Button
-                                                variant='outlined'
-                                                color='primary'
-                                                onClick={this.props.addToBagClickHandler}
-                                            > Add to Bag</Button>
-
-                                            <Button
-                                                variant='outlined'
-                                                color='secondary'
-                                                onClick={this.props.addToWishlistClickHandler}
-                                            > WishList</Button>
-
-                                        </CardActions>
-                                    </Card>
-                                 
-=======
 
                                         {
                                             this.props.clickedId.includes(ele.bookID) ?
                                             <Button
                                                 variant='outlined'
                                                 color='primary'
-                                                onClick={()=>{this.props.addToBagClickHandler(ele.bookID)}}
+                                                onClick={()=>{this.props.addToBagClickHandler(ele.bookID,ele.bookCount)}}
                                             > Added to bag</Button> :
                                             <>
                                             <Button
                                                 variant='outlined'
                                                 color='primary'
-                                                onClick={()=>{this.props.addToBagClickHandler(ele.bookID)}}
+                                                onClick={()=>{this.props.addToBagClickHandler(ele.bookID,ele.bookCount)}}
                                             > Add to bag</Button>
                                             <Button
                                                 variant='outlined'
@@ -124,25 +79,16 @@ class DisplayBooks extends Component {
                                         }
                                         </CardActions>
                                     </Card>
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                                 </>
                             );
                         })
                     }
-<<<<<<< HEAD
-
-                </div>
-                {/* <div className='pagination-div'>
-                    <Pagination count={10} 
-                    color="primary" />
-=======
                 </div>
                 {/* <div className='pagination-div'>
                     <Pagination 
                     count={Math.floor(this.props.bookCount/12)} 
                     color="primary" 
                     onClick={this.props.onChangePaginationHandler} />
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                 </div> */}
             </>
         )
