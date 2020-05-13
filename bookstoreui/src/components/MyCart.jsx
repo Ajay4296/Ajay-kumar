9810eb7b553291973d361879f6acd5faa81d569d
shreyashkaushal <shreyashkaushal10@gmail.com>
2020-05-14 01:37:22 +0530

    [Shreyash] Refactor. refactor login,mycart,ordersummary component after merge error

--- bookstoreui/src/components/MyCart.jsx
+++ bookstoreui/src/components/MyCart.jsx
@@ -2,35 +2,27 @@ import React, { Component } from 'react';
 import { Typography, Button } from '@material-ui/core';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
-<<<<<<< HEAD
-import logo from '../logo.svg';
-=======
 import logo from '../assets/2states.jpg';
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
 
 class MyCart extends Component {
     render() {
         return (
             <div className='my-cart-main-div'>
                 <div className='my-cart-sub-div'>
-                    <Typography variant="h4">My cart (1)</Typography>
-                    <div className='book-image-details-div'>
-                        <div className='book-image-div'>
-<<<<<<< HEAD
-                            <img src={logo} alt='error' />
+                    <Typography variant="h4">My cart ({this.props.cartAddedCount})</Typography>
+                    
+                    {
+                        this.props.cart.map((ele)=>{
+                            return(
+                                <>
+                                <div className='book-image-details-div'>
+                                <div className='book-image-div'>
+                            <img id='img-cart' src={ele.bookImage} alt='error' />
                         </div>
                         <div className='book-details-div'>
-                            <Typography variant="h5" >Book name</Typography>
-                            <Typography>Author name</Typography>
-                            <Typography>price</Typography>
-=======
-                            <img id='img-cart' src={logo} alt='error' />
-                        </div>
-                        <div className='book-details-div'>
-                            <Typography variant="h5" >2 States</Typography>
-                            <Typography>Chetan Bhagat</Typography>
-                            <Typography>₹ 390</Typography>
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
+                            <Typography variant="h5" >{ele.bookTitle}</Typography>
+                            <Typography>{ele.authorName}</Typography>
+                            <Typography>₹ {ele.bookPrice}</Typography>
                             <div className='item-quantity-div'>
                                 <Button>
                                     <RemoveCircleOutlineIcon />
@@ -43,33 +35,31 @@ class MyCart extends Component {
                                 <Button
                                     variant='outlined'
                                     color='secondary'
+                                    onClick={()=>{this.props.deleteCartHandler(ele.cartID)}}
                                 >Remove</Button>
 
                             </div>
                         </div>
                     </div>
+                    </>
+                            )
+                        })
+                    }
+                       
                     <div className='place-order-btn-div'>
-<<<<<<< HEAD
-                        <Button variant='contained' color='primary'>
-=======
                         <Button 
                         variant='contained' 
                         color='primary' 
                         onClick={this.props.placeOrderClickedHandler}>
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                             Place order
                     </Button>
                     </div>
                 </div>
                 <div className='customer-details-div'>
                     <Typography variant="h5">Customer Details</Typography>
-<<<<<<< HEAD
-                    <form action="" className=" p-5" name="myForm" id="f" >
-=======
                     {
                         this.props.showCustomerDetails ?
                         <form action="" className=" p-5" name="myForm" id="f" >
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                         <div className="row">
                             <div className="col">
                                 <div className="form-group">
@@ -117,15 +107,6 @@ class MyCart extends Component {
                             <label>type</label>
                         </div>
                         <div class="form-group form-check" id='check-box-div'>
-<<<<<<< HEAD
-                            <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" /> Home
-                            </label>
-                            <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" /> Work
-                            </label>
-                            <label class="form-check-label">
-=======
                             <label class="form-check-label" id='form-check-label'>
                                 <input class="form-check-input" type="checkbox" /> Home
                             </label>
@@ -133,20 +114,14 @@ class MyCart extends Component {
                                 <input class="form-check-input" type="checkbox" /> Work
                             </label>
                             <label class="form-check-label" id='form-check-label'>
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                                 <input class="form-check-input" type="checkbox" /> other
                             </label>
                         </div>
                         <div className='form-group'>
                             <button type="submit" id="continue" className="btn btn-primary">Continue</button>
                         </div>
-<<<<<<< HEAD
-                    </form>
-
-=======
                     </form> : null 
                     }
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                 </div>
                 <div className='order-summary-div'>
                     <Typography variant="h5">Order summary</Typography>
--- bookstoreui/src/components/OrderSummary.jsx
+++ bookstoreui/src/components/OrderSummary.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { Typography } from '@material-ui/core';
-=======
 import { Typography, Button } from '@material-ui/core';
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
 import orderImage from '../assets/ordersuccessful.jpg'
 
 class OrderSummary extends Component {
@@ -12,11 +8,7 @@ class OrderSummary extends Component {
             <div className='order-main-div'>
                 <Typography variant='h3'>Order Placed Successfully</Typography>
                 <div className='order-image-div'>
-<<<<<<< HEAD
-                <img className='img' src={orderImage}/>
-=======
                 <img className='img' id='img' src={orderImage}/>
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
                 </div>
                 <p><b>Hurray!! your order is confirmed ,<br />
                 the order id is #12112 save the order id for<br />
@@ -37,16 +29,9 @@ class OrderSummary extends Component {
                                 <td>1654/A,Shobha happy homes,<br /> 24th main, 2nd stage,HSR Layout</td>
                             </tr>
                         </tbody>
-<<<<<<< HEAD
-                    </table>
-
-
-                </div>
-=======
                     </table>          
                 </div>
                 <Button variant='outlined' color='primary'>Continue Shopping</Button>
->>>>>>> 94232d33ace64fdab9adbcf9c6338da4cd36410a
             </div>
         )
     }
